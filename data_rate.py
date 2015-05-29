b1b39a698b6cf7375a493cf8f071a40d39817f5b
Jimmy Klein <jimmy.klein4@gmail.com>
2015-05-29 19:29:34 -0400

    Revert "Data Rate shows more information"
    This reverts commit 8888be6d3fef3ebbfefb61a104f24d807e40bb57.

--- data_rate.py
+++ data_rate.py
@@ -1,16 +1,14 @@
 import os
 import re
 
-f = open('testAcrossRoom.txt', 'r')
+f = open('first_plaintext.txt', 'r')
 
 contents = f.read()
 rate = re.compile('Data Rate: [0-9]*\.[0-9]')
-dataRate = re.compile('[0-9]+ bytes on wire')
 allRates = rate.findall(contents)
-allData = dataRate.findall(contents)
 
 l =[]
-p = []
+
 print len(allRates)
 
 for x in range(len(allRates)):
@@ -20,16 +18,8 @@ for x in range(len(allRates)):
         except ValueError:
             pass
 
-
-for x in range(len(allData)):
-    for t in allData[x].split():
-        try:
-            p.append(float(t))
-        except ValueError:
-            pass
-
-print "Total data transmitted"
-print sum(p)/(1024*1024)
-
-print "Average data rate"
+l.sort()
+for i in range (0,5):
+    print l[i]
+    
 print sum(l)/float(len(l))
